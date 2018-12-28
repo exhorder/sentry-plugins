91e51074cc756ead6b3e8b1306aa7c1c2537b0a5
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-12-28 09:13:14 -0600

    ref(webpack): ES6-ify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,52 @@
 /*eslint-env node*/
-/*eslint no-var:0*/
-var path = require('path'),
-  webpack = require('webpack'),
-  CompressionPlugin = require('compression-webpack-plugin'),
-  LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+/*eslint import/no-nodejs-modules:0 */
+const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
-var APPS = [
-  'hipchat-ac',
-  'jira',
-  'sessionstack'
-];
+const APPS = ['hipchat-ac', 'jira', 'sessionstack'];
 
-var IS_PRODUCTION = process.env.NODE_ENV === 'production';
-var WEBPACK_MODE = IS_PRODUCTION ? 'production' : 'development';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const WEBPACK_MODE = IS_PRODUCTION ? 'production' : 'development';
 
 function getConfig(app) {
-  var pyName = app.replace('-', '_');
-  var staticPrefix = 'src/sentry_plugins/' + pyName + '/static/' + pyName,
-      distPath = staticPrefix + '/dist';
+  const pyName = app.replace('-', '_');
+  const staticPrefix = `src/sentry_plugins/${pyName}/static/${pyName}`;
+  const distPath = `${staticPrefix}/dist`;
 
-  var config = {
+  const config = {
     mode: WEBPACK_MODE,
     name: app,
-    entry: './' + pyName + '.jsx',
+    entry: `./${pyName}.jsx`,
     context: path.join(__dirname, staticPrefix),
     externals: {
-      'react': 'React',
+      react: 'React',
       'react-dom': 'ReactDOM',
       'react-router': 'Router',
-      'reflux': 'Reflux',
-      'moment': 'moment',
-      'sentry': 'SentryApp',
+      reflux: 'Reflux',
+      moment: 'moment',
+      sentry: 'SentryApp',
       'prop-types': 'PropTypes',
     },
     module: {
       rules: [
         {
           test: /\.jsx?$/,
-          loader: 'babel-loader',
+          use: 'babel-loader',
           include: path.join(__dirname, staticPrefix),
-          exclude: /(vendor|node_modules)/
-        }
-      ]
+          exclude: /(vendor|node_modules)/,
+        },
+      ],
     },
-    plugins: [
-      new LodashModuleReplacementPlugin(),
-    ],
+    plugins: [new LodashModuleReplacementPlugin()],
     resolve: {
-      extensions: ['*', '.jsx', '.js']
+      extensions: ['*', '.jsx', '.js'],
     },
     output: {
       path: path.join(__dirname, distPath),
-      filename: app + '.js',
-      sourceMapFilename: app + '.js.map',
+      filename: `${app}.js`,
+      sourceMapFilename: `${app}.js.map`,
     },
     devtool: IS_PRODUCTION ? 'source-map' : 'cheap-module-eval-source-map',
   };
@@ -61,15 +55,15 @@ function getConfig(app) {
   // ending in .gz, to be picked up and served by our internal static media
   // server as well as nginx when paired with the gzip_static module.
   if (IS_PRODUCTION) {
-    config.plugins.push(new CompressionPlugin({
-      algorithm: 'gzip',
-      test: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/,
-    }));
+    config.plugins.push(
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/,
+      })
+    );
   }
 
   return config;
 }
 
-module.exports = APPS.map(function(a) {
-  return getConfig(a);
-});
+module.exports = APPS.map(getConfig);
